9f4b1fa8d5adb070ccf78e660810673217fe6bf8
Matti Nelimarkka <matti.nelimarkka@helsinki.fi>
2020-12-31 22:24:31 +0200

    Fix script

--- scripts/install-tcat.py
+++ scripts/install-tcat.py
@@ -61,8 +61,10 @@ config = open( install_path + '/config.php.example', 'r' ).read()
 config = config.replace('$database = "twittercapture";', '$database = "tcat_%s";' % proper_project )
 config = config.replace('$mail_to = "";', '$mail_to = "%s";' % args.admin_email )
 config = config.replace('$dbuser = "";\n$dbpass = "";', '$dbuser = "tcat_%s";\n$dbpass = "%s";' % (proper_project, user_password ) )
-config = config.replace('$twitter_consumer_key = "";\n$twitter_consumer_secret = "";\n$twitter_user_token = ""; \n$twitter_user_secret = "";', '$twitter_consumer_key = "{0}";\n$twitter_consumer_secret = "{1}";\n$twitter_user_token = "{2}"; \n$twitter_user_secret = "{3}";'.format( args.twitter_consumer_key, args.twitter_consumer_secret, args.twitter_user_token, args.twitter_user_secret )
-')
+config = config.replace(
+'$twitter_consumer_key = "";\n$twitter_consumer_secret = "";\n$twitter_user_token = ""; \n$twitter_user_secret = "";',
+'$twitter_consumer_key = "{0}";\n$twitter_consumer_secret = "{1}";\n$twitter_user_token = "{2}"; \n$twitter_user_secret = "{3}";'
+.format( args.twitter_consumer_key, args.twitter_consumer_secret, args.twitter_user_token, args.twitter_user_secret ) )
 
 ## todo: check how to make these sane
 config = config.replace("define('ADMIN_USER', serialize(array('admin', 'admin2')));", "// define('ADMIN_USER', serialize(array('admin', 'admin2')));")
