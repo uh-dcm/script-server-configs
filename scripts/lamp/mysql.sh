664fc90fcfde832f851322b970d649577b8c2bf6
Matti Nelimarkka <matti.nelimarkka@helsinki.fi>
2021-05-12 21:05:56 +0300

    fix typo on variable name

--- scripts/lamp/mysql.sh
+++ scripts/lamp/mysql.sh
@@ -6,7 +6,7 @@ spawn sudo mysql -u root -p
 expect "Enter password: "
 send -- "\r"
 expect "mysql> "
-send -- "CREATE USER 'admin'@'localhost' IDENTIFIED BY '$passwor';\r"
+send -- "CREATE USER 'admin'@'localhost' IDENTIFIED BY '$password';\r"
 expect "mysql> "
 send -- "GRANT ALL PRIVILEGES ON *.* TO 'admin'@'localhost' WITH GRANT OPTION;\r"
 expect "mysql> "
