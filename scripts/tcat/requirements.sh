6f52b96c4dd8ae5a9463c5b70f6e22f4fb07872a
Matti Nelimarkka <matti.nelimarkka@helsinki.fi>
2021-05-12 21:37:44 +0300

    -y will do for apt

--- scripts/tcat/requirements.sh
+++ scripts/tcat/requirements.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 ## install requirements
-sudo apt -yes install php-mbstring libmysqlclient-dev mariadb-plugin-tokudb php-curl
+sudo apt -y install php-mbstring libmysqlclient-dev mariadb-plugin-tokudb php-curl
 ## geo?
 pip3 install -r requirements.txt
