ce781d870822031511ab7bd60249cae8b126530a
Matti Nelimarkka <matti.nelimarkka@helsinki.fi>
2021-05-12 22:18:30 +0300

    More PHP modules and reload apache after installation

--- scripts/tcat/requirements.sh
+++ scripts/tcat/requirements.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 ## install requirements
-sudo apt -y install php-mbstring libmysqlclient-dev mariadb-plugin-tokudb php-curl
+sudo apt -y install php-mbstring libmysqlclient-dev mariadb-plugin-tokudb php-curl php-mysql
 ## geo?
 pip3 install -r requirements.txt
+
+sudo /etc/init.d/apache2 reload
